ac0e7aa4d92a0048614ba4159f082066809a1914
Makeesy AI <makeesyai@gmail.com>
2021-01-11 21:29:11 +0000

    add type casting

--- python_basics/dtypes.py
+++ python_basics/dtypes.py
@@ -2,29 +2,48 @@
 # then it will automatically know whether the value
 # given would be an int, float, or char or even a str.
 
+# Integer
 # var = 123
 # print(var)
 # print(type(var))
 
+# Float
 # var = 123.0
 # print(var)
 # print(type(var))
 
+# String
 # var = 'Hello World'
 # print(var)
 # print(type(var))
 
+# There is No character type is python
 # var = 'H'
 # print(var)
 # print(type(var))
-var = "Hello world"
-var_bytes = bytes(var, encoding='utf8')
-for item in var_bytes:
-    print(item, chr(item))
-print(var_bytes)
-print(type(var_bytes))
-print(var.encode())
-print(var_bytes.decode())
+
+# Bytes type
+# var = "Hello world"
+# var_bytes = bytes(var, encoding='utf8')
+# for item in var_bytes:
+#     print(item, chr(item))
+# print(var_bytes)
+# print(type(var_bytes))
+# print(var.encode())
+# print(var_bytes.decode())
+
+# Type casting
+var = '1234'
+print(type(var))
+var_int = int(var)
+print(var_int)
+print(type(var_int))
+
+# Type casting is only allowed for valid conversion
+var = 234
+var_int = float(var)
+print(var_int)
+print(type(var_int))
 
 # Duck-typing is not dynamic typing:
 # In Duck-typing, any operation doesn't put any formal requirements
