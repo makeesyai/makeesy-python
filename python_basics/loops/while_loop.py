7fe73f20bb88d2a52b379826149e905079593946
Makeesy AI <makeesyai@gmail.com>
2021-01-16 22:31:22 +0000

    Add code snippet for loop and selection

---
+++ python_basics/loops/while_loop.py
@@ -0,0 +1,20 @@
+# WHILE LOOP
+
+# Using else statement with while loops :
+# The else clause is only executed when your while condition becomes false.
+# If you break out of the loop, or if an exception is raised, it won’t be executed.
+
+# Single statement while block
+
+
+
+# FOR LOOP
+# for in Loop:For loops are used for sequential traversal. For example: traversing a list or string or array etc.
+
+# Iterating by index of sequences
+
+# Using else statement with for loops: similar to while loop
+
+# Nested Loops
+
+# Loop Control Statements: continue and break
---
+++ python_basics/selection/if_else.py
@@ -0,0 +1,11 @@
+# if statement
+
+# if..else statements
+
+# nested if statements
+
+# if-elif ladder
+
+# Short Hand if statement
+
+# Short Hand if-else statement
