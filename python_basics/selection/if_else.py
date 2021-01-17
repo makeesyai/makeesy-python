ba6fd79117395c8077a977e1ec43d07f8a44515c
Makeesy AI <makeesyai@gmail.com>
2021-01-17 14:08:36 +0000

    Add code snippet for if else

--- python_basics/selection/if_else.py
+++ python_basics/selection/if_else.py
@@ -1,11 +1,37 @@
 # if statement
+# i = 20
+# if i < 15:
+#     # This is if block
+#     print("I'm less than 15")
+#     if i > 5:
+#         print("I'm greater than 5")
+#     else:
+#         print("I'm less than 5")
+# else:
+#     print("I'm greater than 15")
+#
+# print("Outside if block")
 
 # if..else statements
 
 # nested if statements
 
 # if-elif ladder
+# i = 9
+#
+# if i < 15:
+#     print("I'm less than 15")
+# elif i < 10:
+#     print("I'm less than 10")
+# elif i < 5:
+#     print("I'm less than 5")
+# else:
+#     print("I'm greater 15")
+# print("Outside if else block")
 
 # Short Hand if statement
+i = 20
+# if i < 10: print("I'm less than 10")
+print("I'm less than 10") if i < 10 else print("I'm greater than 10")
 
 # Short Hand if-else statement
