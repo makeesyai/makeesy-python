97320d6ab83e7c074f8a85e8d2dc2fa5e7bcd6d7
Makeesy AI <makeesyai@gmail.com>
2021-01-18 17:17:08 +0000

    Add code updated data structure

--- python_basics/data_structure/tuple.py
+++ python_basics/data_structure/tuple.py
@@ -2,12 +2,19 @@
 # Tuples are usually faster than lists.
 
 # Create empty, (not of much use, as can not be modified)
-var_tuple = tuple()
+# var_tuple = tuple()
 var_tuple = ()
+print(var_tuple)
+print(type(var_tuple))
 
 # Create with values
-
+var_tuple = ("Hello", "Python", 123, 456.4)
+print(var_tuple)
 # get (by indexing)
-
+# item = var_tuple[2]
+# print(item)
+# var_tuple[2] = 400
 # replace(completely)
+var_tuple = (1, 2, 3, 4)
+print(var_tuple)
 # var_tuple[1] = "python"  # ERROR
