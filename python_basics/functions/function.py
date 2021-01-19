47875f6086289e070066f1d0b0432785d9a7c9a8
Raj Nath Patel <patelrajnath@gmail.com>
2021-01-19 11:05:01 +0000

    Add lambda, filter

--- python_basics/functions/function.py
+++ python_basics/functions/function.py
@@ -8,3 +8,26 @@ In Python,Values are passed to function by object reference.
 1. if object is immutable (not modifiable) than the modified value is not available outside the function.
 2. if object is mutable (modifiable) than modified value is available outside the function.
 """
+
+
+def val(first_value, second_value):
+    print('ID in local function for val1:', id(first_value))
+    print('ID in local function for val2:', id(second_value))
+    first_value = second_value + 1  # A new object is created as first_value is immutable
+    print('ID in local function for val1:', id(first_value))
+
+
+def lst(value_lst):
+    new_lst = value_lst  # No new object is created as the value_lst is mutable
+    print(new_lst, id(new_lst))
+
+
+val1 = 123
+val2 = 456
+val(val1, val2)
+print('ID in local for val1:', id(val1))
+print('ID in local for val2:', id(val2))
+
+lst_values = [1, 2, 3, 4, 5]
+lst(lst_values)
+print(lst_values, id(lst_values))
---
+++ python_basics/functions/function_lambda.py
@@ -0,0 +1,11 @@
+# The lambda function has no name and also referred as anonymous function
+# The most important thing to remember wrt lambda function is that it can have multiple arguments
+# But are restricted to have syntactically only one expression, which is evaluated given the arguments and returned
+
+def add(x, y, z=20):
+    return x+y+z
+
+
+add_lambda = lambda x, y, z=10: x+y+z
+
+print(add(2, 3))
