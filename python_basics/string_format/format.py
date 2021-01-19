daf2da9ac18c5c974251d2b58dc311a9049008d9
Makeesy AI <makeesyai@gmail.com>
2021-01-19 15:56:29 +0000

    Add string modulo

--- python_basics/string_format/format.py
+++ python_basics/string_format/format.py
@@ -1,6 +1,15 @@
 a = "Hello"
 b = "World"
 
-print(f'{a} {b}')
+# print(f'{a} {b}')
+
+# 2 points = 1 space
 print('{0:10}, How are the {1:8} is going'.format(a, b))
-print('{0:.1f}%'.format(23.45))
+
+# print('{0:.1f}%'.format(23.45))
+
+# String modulo Format
+print("Geeks : %4d, Portal : %7.2f" %(1, 1.333))
+print("Geeks : %4d, Portal : %7.2f" %(10, 10.333))
+print("Geeks : %4d, Portal : %7.2f" %(100, 100.333))
+print("Geeks : %4d, Portal : %7.2f" %(1000, 1000.333))
