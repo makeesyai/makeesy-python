4ea093ee8bd89bbaf153efc67c9f1e7af5cb07c6
Makeesy AI <makeesyai@gmail.com>
2021-01-19 21:17:20 +0000

    Add code for basic function

--- python_basics/functions/function.py
+++ python_basics/functions/function.py
@@ -4,9 +4,36 @@
 # Illustrate the code repetition with print two lists (integer, strings),
 # then implement the same using function
 # Optional Attributes: arguments, doc string, return statement
+# def print_hello():
+#     """
+#     This function prints hello world
+#     :return:
+#     """
+#     print("Hello world")
+#
+#
+# print_hello()
+# print(print_hello.__doc__)
 
 # Function with Doc String (add doc string and print)
 
 # Scope of variables (define the same variable in global and function scope)
 
+# def print_number():
+    # x = 20
+    # print(x)
+
+
+# x = 10
+# print_number()
+# print(x)
+
 # Function with return value (no arguments)
+
+
+def greet():
+    return f"Hello, {name} Good morning!"
+
+
+name = "Raj Nath Patel"
+print(greet())
