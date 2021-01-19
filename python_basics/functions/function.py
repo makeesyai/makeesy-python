4df64876a92eea79689fa391bcd4530c177c7fde
Makeesy AI <makeesyai@gmail.com>
2021-01-19 21:28:06 +0000

    Add code for basic function

--- python_basics/functions/function.py
+++ python_basics/functions/function.py
@@ -5,42 +5,47 @@
 # then implement the same using function
 # Optional Attributes: arguments, doc string, return statement
 
-# def print_hello():
-#     print("Hello world")
-#
-#
-# print_hello()
+
+def print_hello(): # Bare minimum function definition
+    print("Hello world")
+
+
+print_hello()  # function call without a return value
 
 
 # Function with Doc String (add doc string and print)
 
-# def print_hello():
-#     """
-#     This function prints hello world
-#     :return:
-#     """
-#     print("Hello world")
-#
-#
-# print_hello()
-# print(print_hello.__doc__)
+
+def print_hello():
+    """
+    This function prints hello world
+    :return:
+    """
+    print("Hello world")
+
+
+print(print_hello.__doc__)
+
 
 # Scope of variables (define the same variable in global and function scope)
 
-# def print_number():
-    # x = 20
-    # print(x)
+
+def print_number():
+    x = 20
+    print(x)
 
 
-# x = 10
-# print_number()
-# print(x)
+x = 10
+print_number()  # function call without any return value
+print(x)
 
 # Function with return value (no arguments)
+
 
 def greet():
     return f"Hello, {name} Good morning!"
 
 
 name = "Raj Nath Patel"
-print(greet())
+return_value = greet()  # Call function with return value
+print(return_value)
