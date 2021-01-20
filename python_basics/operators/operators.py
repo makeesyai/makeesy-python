a306c35e4e85688cae42de133f71fde258159552
Raj Nath Patel <patelrajnath@gmail.com>
2021-01-20 18:15:48 +0000

    Add module for operators

---
+++ python_basics/operators/operators.py
@@ -0,0 +1,11 @@
+# Arithmetic operators (+, -, *,  /, //, %, **)
+
+# Relational Operators (>, <, ==, <=, >=, !=)
+
+# Logical operators (and, or, not)
+
+# Assignment operators (=, +=, -=, *=, /=, //=, %=, **=)
+
+# Special operators (Identity: is "x is y, x is not y"; Membership: "x in y, x not in y")
+
+# Operator Precedence (BODMAS)
