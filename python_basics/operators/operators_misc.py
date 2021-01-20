435015f15dae04587a542d9e4b124184eca38188
Raj Nath Patel <patelrajnath@gmail.com>
2021-01-20 18:48:07 +0000

    Add module for operators

---
+++ python_basics/operators/operators_misc.py
@@ -0,0 +1,7 @@
+# Logical operators (and, or, not)
+
+# Any: Returns True if any item in iterator is True. Returns false if empty or all False
+
+# All: Returns true if all the items in an iterator is True or if empty. Returns False any item if False
+
+# Special operators (Identity: is "x is y, x is not y"; Membership: "x in y, x not in y")
