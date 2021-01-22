b85aee9339b895a06050f28332b67dd250df2090
Makeesy AI <makeesyai@gmail.com>
2021-01-22 22:54:01 +0000

    String formatter

---
+++ python_basics/string_format/format_advance.py
@@ -0,0 +1,5 @@
+# f-strings.
+# Template strings.
+name="Raj"
+last_name = "\n\rPatel"
+print(f'{name} {last_name}')
---
+++ python_basics/string_format/format_general.py
@@ -0,0 +1,20 @@
+# Why we need string formatting?
+# The modulo operator.
+# The format function.
+
+# String modulo Format
+print("Geeks : %4d, Portal : %7.2f" %(1, 1.333))
+print("Geeks : %4d, Portal : %7.2f" %(10, 10.333))
+print("Geeks : %4d, Portal : %7.2f" %(100, 100.333))
+print("Geeks : %4d, Portal : %7.2f" %(1000, 1000.333))
+
+# Example Single Formatter
+a = "Hello"
+b = "World"
+
+# 2 points = 1 space
+print('{0:10}, How are the {1:8} is going'.format(a, b))
+
+# print('{0:.1f}%'.format(23.45))
+
+
