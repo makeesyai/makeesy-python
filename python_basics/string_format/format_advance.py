c445aafb03d5e1ebb50701a66e6163cf377ba6e5
Makeesy AI <makeesyai@gmail.com>
2021-01-23 17:09:08 +0000

    String formatter

--- python_basics/string_format/format_advance.py
+++ python_basics/string_format/format_advance.py
@@ -1,5 +1,7 @@
 # f-strings.
 # Template strings.
-name="Raj"
+name = "Raj"
 last_name = "\n\rPatel"
 print(f'{name} {last_name}')
+a = 123.40
+print(f'The number is: {a:f}')
--- python_basics/string_format/format_general.py
+++ python_basics/string_format/format_general.py
@@ -1,20 +1,46 @@
 # Why we need string formatting?
+# Example: sum of two numbers
+import math
+import sys
+
+a = 4.000000000000001
+b = 3000.0
+print("%10.100f" % a)
+added = a + b
+
+"""
+Usage1: Formatting Logs
+Historically, for float numbers, the Python prompt and built-in repr()/print() function would choose 
+the one with 17 significant digits (including .)
+"""
 # The modulo operator.
-# The format function.
+# print("The sum of %f, and %f is: %f" % (a, b, added))
 
-# String modulo Format
-print("Geeks : %4d, Portal : %7.2f" %(1, 1.333))
-print("Geeks : %4d, Portal : %7.2f" %(10, 10.333))
-print("Geeks : %4d, Portal : %7.2f" %(100, 100.333))
-print("Geeks : %4d, Portal : %7.2f" %(1000, 1000.333))
+# Formatters
+# Example with single place holders.
+# print("The sum is {}, {}, {}".format(a, b, added))
 
-# Example Single Formatter
-a = "Hello"
-b = "World"
-
-# 2 points = 1 space
-print('{0:10}, How are the {1:8} is going'.format(a, b))
-
-# print('{0:.1f}%'.format(23.45))
+# Example with multiple place holders.
+# print('The sum of {:f}, and {:} is: {:f}'.format(a, b, added))
+# print('{}'.format(23.45))
 
 
+# Example with Positional and Keyword Arguments
+# print('The sum of {0}, and {1} is: {2}'.format(a, b, added))
+# print('The sum of {x}, and {y} is: {z}'.format(x=a, y=b, z=added))
+
+"""
+Usage2: Data Representation
+"""
+
+# Spacing with String modulo
+# print("Hello : %4d, World : %7.2f" % (1, 1.333))
+# print("Hello : %4d, World : %7.2f" % (10, 10.333))
+# print("Hello : %4d, World : %7.2f" % (100, 100.333))
+# print("Hello : %4d, World : %7.2f" % (1000, 1000.333))
+
+# Spacing with format (note: default format for floating point is- "e")
+# print("Hello : {0:4}, World : {1:7.3}".format(1, 1.333333))
+# print("Hello : {0:4}, World : {1:7.3}".format(10, 145.51))
+# print("Hello : {0:4}, World : {1:7.3}".format(100, 3456.333))
+# print("Hello : {0:4}, World : {1:7.3}".format(1000, 4567.333))
