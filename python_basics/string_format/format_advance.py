0fd77c66cccc32fd680bc874a0cf20afcad170a0
Makeesy AI <makeesyai@gmail.com>
2021-01-24 21:58:15 +0000

    String formatter

--- python_basics/string_format/format_advance.py
+++ python_basics/string_format/format_advance.py
@@ -1,7 +1,67 @@
-# f-strings.
+# f/F-strings.
+# Simple Syntax
+a = 45
+b = 46
+added = a + b
+
+
+def my_sum(x, y):
+    return x + y
+
+
+# print(f'The sum of {a}, and {b} is: {added}')
+# print(F'The sum of {a}, and {b} is: {my_sum(a,b)}')
+# print(F'The sum of {a}, and {b} is: {a + b}')
+# print(F'The sum of {a}, and {b} is: {a*b}')
+
+print(f'The sum of {a}, and {b} is: {added} \n'
+      f'The sum of {a}, and {b} is: {my_sum(a,b)} \n'
+      f'The sum of {a}, and {b} is: {a*b}')
+
+# Arbitrary Expressions
+# Multiline f-Strings
+
+# Speed
+# import timeit
+# time_taken = timeit.timeit("""
+# name = "Eric"
+# age = 74
+# '%s is %s.' % (name, age)
+# """, number=10000)
+# print('Modulo:', time_taken)
+#
+# time_taken = timeit.timeit("""
+# name = "Eric"
+# age = 74
+# '{} is {}.'.format(name, age)
+# """, number=10000)
+# print('Format:', time_taken)
+#
+# time_taken = timeit.timeit("""
+# name = "Eric"
+# age = 74
+# f'{name} is {age}.'
+# """, number=10000)
+# print('fstring:', time_taken)
+
+# Remember
+# Quotation Marks
+print(f"The sum of '{a}' and '{b}' is: '{a+b}'")
+# Dictionaries
+math = {'a': a, 'b': b, "sum": added}
+print(f"The sum of \"{math['a']}\" and \"{math['b']}\" is: \"{math['sum']}\"")
+
+# Braces
+print(f'The sum of {{{ {a} }}} and {{{ {b} }}} is: {a+b}')
+
 # Template strings.
-name = "Raj"
-last_name = "\n\rPatel"
-print(f'{name} {last_name}')
-a = 123.40
-print(f'The number is: {a:f}')
+from string import Template
+info = {'a': a, "b": b, "sum": added}
+template = Template("The sum of $a and $b is: $sum")
+print(template.substitute(a=a, b=b, sum=added))
+print(template.safe_substitute(a=a, b=b))
+
+# CONCLUSION
+# User-obtained: Template
+# python> 3.6+: f'string
+# Elsewhere use format method
