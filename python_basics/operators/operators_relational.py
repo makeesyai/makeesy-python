a4addb473e4847596afc0b4db1d2aab155c2a8c8
Raj Nath Patel <patelrajnath@gmail.com>
2021-01-28 16:13:17 +0000

    add code for rel operator

--- python_basics/operators/operators_relational.py
+++ python_basics/operators/operators_relational.py
@@ -1,3 +1,15 @@
+# These operators are used to compare two operands and returns either True
+# or False according to the condition used.
 # Relational Operators (>, <, ==, <=, >=, !=)
-
 # Relational Operator Functions [gt, lt, eq, le, ge, ne]
+
+a = 1
+b = 1
+
+
+# String comparison with relational operator
+# The comparison is performed using the characters in both strings.
+# a = 'bb'
+# b = 'ba'
+
+# Tips: for boolean value don't use comparison
