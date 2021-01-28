ac5c7040d8486f41160370d7449caa756b5386cb
Makeesy AI <makeesyai@gmail.com>
2021-01-28 23:29:42 +0000

    Misc operators

--- python_basics/operators/operators_misc.py
+++ python_basics/operators/operators_misc.py
@@ -1,5 +1,7 @@
 # Logical operators (and, or, not)
 # OR: Short-Circuit Evaluation
+from fractions import Fraction
+
 a = 1
 b = 2
 
@@ -14,9 +16,8 @@ print(f'Exp2:{exp2}, and the negation of exp2:{not exp2}')
 
 # Built-in objects considered false:
 # 1. constants defined to be false: None and False.
-# 2. zero of any numeric type: 0, 0.0, 0j, Decimal(0), Fraction(0, 1)
+# 2. zero of any numeric type: 0, 0.0, 0j Decimal(0), Fraction(0, 1)
 # 3. empty sequences and collections: '', (), [], {}, set(), range(0)
-
 
 # Special operators
 # Interning in Python: The objects that Python does interning
