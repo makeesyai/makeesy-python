e73587ad4501074bffdc54b393a15319354153bd
Makeesy AI <makeesyai@gmail.com>
2021-01-28 18:07:20 +0000

    Final code for rel opt

--- python_basics/operators/operators_relational.py
+++ python_basics/operators/operators_relational.py
@@ -2,14 +2,49 @@
 # or False according to the condition used.
 # Relational Operators (>, <, ==, <=, >=, !=)
 # Relational Operator Functions [gt, lt, eq, le, ge, ne]
-
-a = 1
+import operator
+a = 3
 b = 1
+# > greater than
+#  == equal to
+# < less than
+print(a > b)
+print(operator.gt(a, b))
+print()
 
+print(a < b)
+print(operator.lt(a, b))
+print()
 
+print(a == b)
+print(operator.eq(a, b))
+print()
+
+print(a != b)
+print(operator.ne(a, b))
+print()
+
+print(a <= b)
+print(operator.le(a, b))
+print()
+
+print(a >= b)
+print(operator.ge(a, b))
+print()
+
+print(a <= b)
+if a < b or a == b:
+    print("Less than or equal to operator")
+print(operator.le(a, b))
+print()
 # String comparison with relational operator
 # The comparison is performed using the characters in both strings.
-# a = 'bb'
-# b = 'ba'
-
+a = 'bb'
+b = 'ba'
+print(ord('b'), ord('a'))
+print(a < b)
+print()
 # Tips: for boolean value don't use comparison
+x = True
+if x:
+    print(f"Boolean value is:{x}")
