8e7189c199ab3bab28ac44fff06296f27617e172
Makeesy AI <makeesyai@gmail.com>
2021-01-29 21:31:42 +0000

    Misc operators

--- python_basics/operators/operators_identity.py
+++ python_basics/operators/operators_identity.py
@@ -1,24 +1,3 @@
-# Logical operators (and, or, not)
-# OR: Short-Circuit Evaluation
-from fractions import Fraction
-
-a = 1
-b = 2
-
-exp1 = a > b
-print(f'The exp1: {exp1}')
-exp2 = a != 2
-print(f'The exp2: {exp2}')
-
-print(exp1 and exp2)
-print(exp1 or exp2)
-print(f'Exp2:{exp2}, and the negation of exp2:{not exp2}')
-
-# Built-in objects considered false:
-# 1. constants defined to be false: None and False.
-# 2. zero of any numeric type: 0, 0.0, 0j Decimal(0), Fraction(0, 1)
-# 3. empty sequences and collections: '', (), [], {}, set(), range(0)
-
 # Special operators
 # Interning in Python: The objects that Python does interning
 # on them are- Immutable objects- integer, float, booleans, bytes, tuple,
@@ -42,9 +21,3 @@ x = "Hello World" * 372  # + will disable the interning
 y = "Hello World" * 372
 print(len(x), len(y))
 print(x is y)
-
-
-# Membership: "x in y, x not in y"
-
-# Any: Returns True if any item in iterator is True. Returns false if empty or all False
-# All: Returns true if all the items in an iterator is True or if empty. Returns False any item if False
--- python_basics/operators/operators_logical.py
+++ python_basics/operators/operators_logical.py
@@ -1,20 +1,38 @@
 # Logical operators (and, or, not)
-# OR: Short-Circuit Evaluation
-from fractions import Fraction
-
 a = 1
 b = 2
+exp1 = a < b
+print(f"The exp1 is {exp1}")
+exp2 = a > b
+print(f"The exp2 is {exp2}")
+exp3 = a != b
+print(f"The exp3 is {exp3}")
 
-exp1 = a > b
-print(f'The exp1: {exp1}')
-exp2 = a != 2
-print(f'The exp2: {exp2}')
-
+print()
 print(exp1 and exp2)
+print()
 print(exp1 or exp2)
-print(f'Exp2:{exp2}, and the negation of exp2:{not exp2}')
+print()
+print(not a)
+print((not a) == b)
+print()
 
-# Built-in objects considered false:
+# Built-in objects considered false (other than these all are true):
 # 1. constants defined to be false: None and False.
 # 2. zero of any numeric type: 0, 0.0, 0j Decimal(0), Fraction(0, 1)
 # 3. empty sequences and collections: '', (), [], {}, set(), range(0)
+
+x = set()
+if x:
+    print("The if condition is True")
+else:
+    print("The if condition is False")
+
+
+# AND/OR: Short-Circuit Evaluation
+# Logical "and", and "or" operators in Python returns
+# an operand's value whereas other
+# language like C/C++ operators &&, and || returns 0 and 1
+
+print(0 or 234 or {} or "This is true")  # print 234
+print(345 and 234 and "True" and "This is true")  # print "This is true"
