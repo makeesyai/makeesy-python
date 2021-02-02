b19fb21b33bb30176c674723ee526959438a9238
Makeesy AI <makeesyai@gmail.com>
2021-02-02 12:42:56 +0000

    Updated code for any/all operator.

--- python_basics/operators/operators_all_any.py
+++ python_basics/operators/operators_all_any.py
@@ -1,6 +1,6 @@
 # any/all are extensions of or/and.
 # Any: Returns True if any item in iterator is True.
-# Returns false if empty or all False
+# Returns false if empty or all False (chain of or operations)
 
-# All: Returns true if all the items in an
-# iterator is True or if empty. Returns False any item if False
+# All: Returns true if all the items in an iterator is
+# True or if empty. Returns False any item if False(chain of and operations)
