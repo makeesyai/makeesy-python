9c1f72c6f1fbfb124be1868595ece7f8886d85c2
Makeesy AI <makeesyai@gmail.com>
2021-02-03 20:40:17 +0000

    Updated code for any and all operators operator.

--- python_basics/operators/operators_all_any.py
+++ python_basics/operators/operators_all_any.py
@@ -4,3 +4,40 @@
 
 # All: Returns true if all the items in an iterator is
 # True or if empty. Returns False any item if False(chain of and operations)
+
+def custom_any(seq):
+    for item in seq:
+        if item:
+            return item
+    return False
+
+
+def custom_all(seq):
+    for item in seq:
+        if not item:
+            return item
+    return True
+
+
+a = []
+print(any(a))
+print(all(a))
+print()
+
+a = [0, (), 1, 9]
+print(any(a))
+print(all(a))
+print()
+
+a = [0, (), 1, 9]
+print(custom_any(a))
+print(custom_all(a))
+print()
+
+lst = [1, 3, 5, 6, 7, 5]
+
+boolean_list = []
+for item in lst:
+    boolean_list.append(item % 3 == 0)
+print(boolean_list)
+print(any(boolean_list))
