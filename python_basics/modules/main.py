cb957ba0f189f12058f8bf37ee974a346951600a
Makeesy AI <makeesyai@gmail.com>
2021-02-04 18:15:15 +0000

    Add code module video.

---
+++ python_basics/modules/main.py
@@ -0,0 +1,11 @@
+# Module: a module is a file containing python code with .py ext.
+# Package: a directory with collection of modules and __init__.py.
+
+# import p1.p2.module
+# CALL: p1.p2.module.func()
+# from p1.p2 import module
+# CALL module.func()
+# from p1.p2.module import func
+# CALL: func()
+# from p1.p2.module import func as f
+# CALL: f()
