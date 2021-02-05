d0cf57813fe10f559d30277b0926829d674b60a3
Makeesy AI <makeesyai@gmail.com>
2021-02-05 21:35:03 +0000

    Add pckages code

--- python_basics/packages/main.py
+++ python_basics/packages/main.py
@@ -1,6 +1,10 @@
-# Package: a directory with collection of modules and __init__.py.
-# import p1.p2.module
-# CALL: p1.p2.module.func()
+# Package: is a directory with collection of modules and __init__.py.
+
+# Add resources in __init__.py
+# 3.3+ there is no need to have __init__.py to make a dir package
+
+# import p1.module
+# CALL: p1.module.func()
 # from p1.p2 import module
 # CALL module.func()
 # from p1.p2.module import func
