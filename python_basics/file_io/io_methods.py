9d075a9bb41f2f37c527b848bb9b479582794069
Makeesy AI <makeesyai@gmail.com>
2021-02-07 21:28:28 +0000

    Create modules for io video

---
+++ python_basics/file_io/io_methods.py
@@ -0,0 +1,7 @@
+# NEW LINE
+# WINDOWS: Carriage Return (CR or \r) and the Line Feed (LF or \n) characters (CR+LF or \r\n)
+# LINUX and MAC: Line Feed (LF or \n)
+
+# Character Encodings
+# ASCII: 128 characters
+# Unicode: 1,114,112 characters
