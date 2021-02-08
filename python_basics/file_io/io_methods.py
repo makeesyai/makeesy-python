40184b3f2ea4fc87274145d1f8e94693c3090797
Makeesy AI <makeesyai@gmail.com>
2021-02-08 13:58:00 +0000

    Create modules for io video

--- python_basics/file_io/io_methods.py
+++ python_basics/file_io/io_methods.py
@@ -1,7 +1,24 @@
+# File Types in Python
+# 1. Text file
+# 2. Binary file
+# 3. Raw Binary file (typically not used)
+
+# modes:
+# r (default): open for reading
+# r+: open for reading and writing
+# w: open for writing, creates file if not there, if there truncates it
+# w+: open for reading and writing, creates file if not there, if there truncates it
+# a: open for writing, creates file if not there, if there places the pointer at the end of the file
+# a+: open for reading and writing, creates file if not there,
+# if there place the pointer at the end of the file
+
+# filetypes: binary: b, text: t (default)
+
+
+# Tips
 # NEW LINE
 # WINDOWS: Carriage Return (CR or \r) and the Line Feed (LF or \n) characters (CR+LF or \r\n)
 # LINUX and MAC: Line Feed (LF or \n)
-
 # Character Encodings
 # ASCII: 128 characters
 # Unicode: 1,114,112 characters
