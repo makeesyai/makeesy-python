a0e40561cb984590905e3dd03ff126f3e6df501d
Makeesy AI <makeesyai@gmail.com>
2021-02-09 20:29:29 +0000

    Create modules for io video

---
+++ python_basics/exceptions/exception.py
@@ -0,0 +1,15 @@
+# Exceptions vs Syntax error
+# raise, and assert
+# Simple try-except
+# Catch named exceptions
+# try catch with else
+# Use Exception class
+# try catch with finally
+
+import sys
+
+x = 10
+
+if x > 5:
+    raise Exception("The value is greater than 5")
+
