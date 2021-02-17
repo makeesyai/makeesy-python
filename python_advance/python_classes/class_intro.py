4acbf1a0cddc17b405bea7c1e283a87c760e69aa
Makeesy AI <makeesyai@gmail.com>
2021-02-17 12:33:15 +0000

    Add code for class video

--- python_advance/python_classes/class_intro.py
+++ python_advance/python_classes/class_intro.py
@@ -1,10 +1,17 @@
 # Class: Classes provide a means of bundling data and functionality together.
+# Class variable vs instance variable
 
 class Dog:
+    """
+    Here we add the description for the class
+    """
+    counter = 0
+
     def __init__(self, name, age, color):
         self.name = name
         self.age = age
         self.color = color
+        Dog.counter += 1
 
     def greet(self):
         return f"Hello, {self.name}"
@@ -28,9 +35,11 @@ class DogType(Dog):
         return self.color
 
 
-d1 = DogType('lola', 1, 'brown', 'bulldog')
+d1 = Dog('lola', 1, 'brown')
+d1.__class__.counter = 20
+print(Dog.counter)
 print(d1.greet())
-print(d1.get_age())
-print(d1.get_name())
-print(d1.get_color())
-print(d1.type())
+# print(d1.get_age())
+# print(d1.get_name())
+# print(d1.get_color())
+# print(d1.type())
