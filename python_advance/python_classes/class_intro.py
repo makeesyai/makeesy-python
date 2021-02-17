d3a930ea9b7e66c509de208a15ad288b43f22194
Makeesy AI <makeesyai@gmail.com>
2021-02-17 12:16:39 +0000

    Add code for class video

--- python_advance/python_classes/class_intro.py
+++ python_advance/python_classes/class_intro.py
@@ -6,5 +6,31 @@ class Dog:
         self.age = age
         self.color = color
 
-    def count(self):
-        pass
+    def greet(self):
+        return f"Hello, {self.name}"
+
+
+class DogType(Dog):
+    def __init__(self, name, age, color, breed):
+        super(DogType, self).__init__(name, age, color)
+        self.breed = breed
+
+    def type(self):
+        return self.breed
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def get_color(self):
+        return self.color
+
+
+d1 = DogType('lola', 1, 'brown', 'bulldog')
+print(d1.greet())
+print(d1.get_age())
+print(d1.get_name())
+print(d1.get_color())
+print(d1.type())
