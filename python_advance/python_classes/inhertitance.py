a2ca8173ebebc835203bb7b83789c91fc002d23b
Makeesy AI <makeesyai@gmail.com>
2021-02-24 18:50:58 +0000

    Add code related inheritance

--- python_advance/python_classes/inhertitance.py
+++ python_advance/python_classes/inhertitance.py
@@ -1,14 +1,48 @@
-from abc import ABCMeta
+# Simple Inheritance
+# Multilevel Inheritance
+# Multiple inheritance
+
+class BaseA(object):
+    def __init__(self):
+        pass
+
+    def hi_from_baseA_class(self):
+        print("Hi from BaseA class")
+
+class BaseB(object):
+    def __init__(self):
+        pass
+
+    def hi_from_baseB_class(self):
+        print("Hi from BaseB class")
 
 
-class Dog(object):
-    def __init__(self, a):
-        print('init method is called...', a)
+class ExtendedClass(BaseA):
+    def __init__(self):
+        super(ExtendedClass, self).__init__()
+        pass
 
-    def __new__(cls, *args, **kwargs):
-        print("New method is called...")
-        return super(Dog, cls).__new__(cls)
+    def hi_from_extended_class(self):
+        print("Hi from Extended class...")
+        
+        
+class MultilevelInheritance(ExtendedClass):
+    def __init__(self):
+        super(MultilevelInheritance, self).__init__()
+        pass
+
+    def hi_from_mulitilevel_class(self):
+        print('hi from multilevel class...')
 
 
-d = Dog("Hello")
-print()
+class MultipleInheritance(BaseA, BaseB):
+    def __init__(self):
+        super(MultipleInheritance, self).__init__()
+        pass
+
+
+base = BaseA()
+base.hi_from_baseA_class()
+extended = ExtendedClass()
+extended.hi_from_baseA_class()
+extended.hi_from_extended_class()
