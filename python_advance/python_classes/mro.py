9a9b6d14fcbd2c790b4ce30c2ad545889223986b
Makeesy AI <makeesyai@gmail.com>
2021-03-17 18:29:16 +0000

    Add code for magic functions

--- python_advance/python_classes/mro.py
+++ python_advance/python_classes/mro.py
@@ -1,29 +1,31 @@
 class A(object):
-    def __init__(self, a, **kwargs):
+    def __init__(self, a, *args, **kwargs):
         self.a = a
-        print(kwargs)
-        super(A, self).__init__(**kwargs)
+        print(args, kwargs)
+        super(A, self).__init__(*args, **kwargs)
 
     def hello(self):
         print(f'hello from A, a={self.a}')
 
 
 class B(object):
-    def __init__(self, b):
+    def __init__(self, b, *args, **kwargs):
+        print(args, kwargs)
         self.b = b
+        super(B, self).__init__(*args, **kwargs)
 
     def hello(self):
         print(f'hello from B, b={self.b}')
 
 
-class C(A, B):
-    def __init__(self, **kwargs):
+class C(B, A):
+    def __init__(self, *args, **kwargs):
         # kwargs['a'] = a
         # kwargs['b'] = b
-        print(kwargs)
-        super(C, self).__init__(**kwargs)
+        print(args, kwargs)
+        super(C, self).__init__(*args, **kwargs)
 
 
-c = C(a=2, b=3)
+c = C(2, 3)
 print(c.a)
 print(c.b)
