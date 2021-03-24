db981e33b088036b52a7587b0e0914b90b57c92d
Makeesy AI <makeesyai@gmail.com>
2021-03-24 15:21:47 +0000

    super method

--- python_advance/python_classes/super_method_with_multiple_inheritance.py
+++ python_advance/python_classes/super_method_with_multiple_inheritance.py
@@ -1,27 +1,23 @@
 # Multiple inheritance with supper method
-# Create A, and B, then inherit both in extended class
-# 1. Test init without supper()
-# 2. Add super in first class in the inheritance and print the consumption of the arguments
-# 3. Change the order of the classes in inheritance order,
-# for example if it was (A,B) change it to (B, A), it'll fail
-# 4. Change the order of arguments and fix with keyword arguments
-# 5. Add C as parent class to A, and now pass 3 arguments, show that MRO uses
-# C3 Linearization Algorithm to resolve the methods and attributes)
+
+# Why supper instead of Class name based initialization
+# 1. In case of changes in init method of parent class the subclass init needs to change
+# 2. As a new class is added you need to add a new CLASS.__init__() for that
 
 class C(object):
     def __init__(self, c, *args, **kwargs):
         self.c = c
-        print(args, kwargs)
         super(C, self).__init__(*args, **kwargs)
 
     def hello(self):
-        print(f'hello from C, a={self.c}')
+        print(f'hello from A, a={self.c}')
 
 
 class A(C):
     def __init__(self, a, *args, **kwargs):
         self.a = a
-        print(args, kwargs)
+        print(args)
+        print(kwargs)
         super(A, self).__init__(*args, **kwargs)
 
     def hello(self):
@@ -30,8 +26,9 @@ class A(C):
 
 class B(C):
     def __init__(self, b, *args, **kwargs):
-        print(args, kwargs)
         self.b = b
+        print(args)
+        print(kwargs)
         super(B, self).__init__(*args, **kwargs)
 
     def hello(self):
@@ -40,7 +37,6 @@ class B(C):
 
 class Extended(B, A):
     def __init__(self, *args, **kwargs):
-        print(args, kwargs)
         super(Extended, self).__init__(*args, **kwargs)
 
 
@@ -49,4 +45,3 @@ print(extended.a)
 print(extended.b)
 print(extended.c)
 extended.hello()
-print(Extended.mro())
