302ae0e8c889a2b823ad7daa5f1f84d442627d0d
Makeesy AI <makeesyai@gmail.com>
2021-04-21 16:38:42 +0100

    Add assignments

--- python_advance/python_classes/access_modifiers.py
+++ python_advance/python_classes/access_modifiers.py
@@ -9,14 +9,24 @@ class Person(object):
         self._age = age
         self.__gender = gender
 
+    def _print_gender(self):
+        print('The gender of the person is', self.__gender)
+
 
 class Employee(Person):
-    def __init__(self, name, age, gender, employee_id):
+    def __init__(self, name, age, gender, eid):
         super(Employee, self).__init__(name, age, gender)
-        self.eid = employee_id
+        self.eid = eid
+
+    def print_age(self):
+        print('Age of the employee is', self._age)
 
 
-p1 = Employee('Raj Nath', 21, 'male', 1)
-print(p1.name)
-print(p1._age)
-print(p1._Person__gender)
+e1 = Employee('Raj Nath Patel', 31, 'male', 1)
+print(e1._age)
+print(e1._Person__gender)
+
+# print(e1.name)
+# e1.print_age()
+# e1.print_gender()
+
