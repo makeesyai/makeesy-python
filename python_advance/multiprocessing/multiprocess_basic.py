db444e5875bbd6799442de0d21937a8ce75658ed
Makeesy AI <makeesyai@gmail.com>
2021-05-12 15:38:07 +0100

    Add code for multiprocessing

--- python_advance/multiprocessing/multiprocess_basic.py
+++ python_advance/multiprocessing/multiprocess_basic.py
@@ -1,19 +1,27 @@
+import time
 from multiprocessing import Process
-from time import sleep
 
 
-def worker(identifier):
-    print(f"The process for:{identifier}")
-    sleep(5)
+def cpu_bound(n, name):
+    print(f'Process:{name} started...')
+    while n:
+        n -= 1
+    print(f'Process:{name} Completed!')
 
 
 if __name__ == '__main__':
+    start = time.time()
 
-    p1 = Process(target=worker, args=("p1", ))
-    p2 = Process(target=worker, args=("p2", ))
-    p3 = Process(target=worker, args=("p3", ))
-    p4 = Process(target=worker, args=("p4", ))
+    n = 100000000
+    # cpu_bound(n, 'Call-1')
+    # cpu_bound(n, 'Call-2')
+    # cpu_bound(n, 'Call-3')
+    # cpu_bound(n, 'Call-4')
 
+    p1 = Process(target=cpu_bound, args=(n, "p1", ))
+    p2 = Process(target=cpu_bound, args=(n, "p2", ))
+    p3 = Process(target=cpu_bound, args=(n, "p3", ))
+    p4 = Process(target=cpu_bound, args=(n, "p4", ))
     p1.start()
     p2.start()
     p3.start()
@@ -33,4 +41,7 @@ if __name__ == '__main__':
     p3.close()
     p4.close()
     print('process closed...')
+
+    print(f'The program took: {time.time() - start}')
+
 
