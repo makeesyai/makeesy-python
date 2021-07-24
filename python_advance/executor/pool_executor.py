20400950aa0cf8e06b7c084211ce6f4c883e767b
Makeesy AI <makeesyai@gmail.com>
2021-07-24 08:31:03 +0100

    Add code for pool executor

--- python_advance/executor/pool_executor.py
+++ python_advance/executor/pool_executor.py
@@ -13,28 +13,31 @@ def cpu_bound(n):
 
 
 if __name__ == '__main__':
-    start = time.time()
     sec = 5
     num = 200000000
-    # with ThreadPoolExecutor(max_workers=4) as executor:
-        # e1 = executor.submit(io_bound, sec, )
-        # e2 = executor.submit(io_bound, sec, )
-        # e3 = executor.submit(io_bound, sec, )
-        # e4 = executor.submit(io_bound, sec, )
-        # e1 = executor.submit(cpu_bound, num, )
-        # e2 = executor.submit(cpu_bound, num, )
-        # e3 = executor.submit(cpu_bound, num, )
-        # e4 = executor.submit(cpu_bound, num, )
-        # print(e1.result())
 
-    with ProcessPoolExecutor(max_workers=4) as executor:
+    start = time.time()
+    with ThreadPoolExecutor(max_workers=8) as executor:
         # e1 = executor.submit(io_bound, sec, )
         # e2 = executor.submit(io_bound, sec, )
         # e3 = executor.submit(io_bound, sec, )
         # e4 = executor.submit(io_bound, sec, )
-        e1 = executor.submit(cpu_bound, num, )
-        e2 = executor.submit(cpu_bound, num, )
-        e3 = executor.submit(cpu_bound, num, )
-        e4 = executor.submit(cpu_bound, num, )
+        t1 = executor.submit(cpu_bound, num, )
+        t2 = executor.submit(cpu_bound, num, )
+        t3 = executor.submit(cpu_bound, num, )
+        t4 = executor.submit(cpu_bound, num, )
         # print(e1.result())
-    print(f'The time taken={time.time() - start}')
+    print(f'The time taken by thread pool={time.time() - start}')
+
+    start = time.time()
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        # e1 = executor.submit(io_bound, sec, )
+        # e2 = executor.submit(io_bound, sec, )
+        # e3 = executor.submit(io_bound, sec, )
+        # e4 = executor.submit(io_bound, sec, )
+        p1 = executor.submit(cpu_bound, num, )
+        p2 = executor.submit(cpu_bound, num, )
+        p3 = executor.submit(cpu_bound, num, )
+        p4 = executor.submit(cpu_bound, num, )
+        # print(e1.result())
+    print(f'The time taken by process pool={time.time() - start}')
